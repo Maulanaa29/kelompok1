733054190e1cb8b86a986e2a8b02e79df2b85392
awngsptra <awngsptra@gmail.com>
2024-07-12 21:04:58 +0800

    Menerapkan fungsi perhitungan rata-rata bobot

--- codingan.cpp
+++ codingan.cpp
@@ -13,3 +13,9 @@ struct Siswa {
 float Hitung_Nilai_Akhir(Siswa siswa) {
     return (siswa.NilaiTugas * 0.3) + (siswa.NilaiKuis * 0.2) + (siswa.NilaiUjian * 0.5);
 }
+
+void Hitung_RataRata_Bobot(Siswa siswa) {
+    float RataRataBobot = (siswa.NilaiTugas + siswa.NilaiKuis + siswa.NilaiUjian) / 3.0;
+    cout << "Rata-Rata Bobot " << siswa.nama << " Adalah " << RataRataBobot << endl;
+}
+
